2293af3a54ab53946da8ba4ffecc25a07be826d8
fancui <fancui408@gmail.com>
2017-02-13 16:33:31 -0800

    all

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,13 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+end
+
+def new
+   @group = Group.new
+ end
+end
+
 
   def index
     @groups = Group.all
